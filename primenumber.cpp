a923ab925572e0a076df8ad7aafa0f514ae2056f
Zakia-Sultana084 <zakiasultana223400052@gmail.com>
2025-02-23 09:50:13 +0600

    modified

--- primenumber.cpp
+++ primenumber.cpp
@@ -1,10 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-//write a function to print prime numbers from 1 to 100 and call it in the main function
+bool isPrime(int num) {
+    if (num <= 1) {
+        return false;
+    }
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void printPrimes() {
+    for (int i = 1; i <= 100; i++) {
+        if (isPrime(i)) {
+            cout << i << " ";
+        }
+    }
+    cout << endl;
+}
 
 int main() {
-
-    //call the function here
+    printPrimes();
     return 0;
 }
